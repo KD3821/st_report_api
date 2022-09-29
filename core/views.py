fc015cd6fbb07608d2ec7be990470953da1697e7
KD3821 <80853241+KD3821@users.noreply.github.com>
2022-09-29 16:42:02 +0300

    ordering&filtering added

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from core.models import Week, Shift, Car, Driver, ExtraTax, Ride
 from core.serializers import WeekSerializer, ShiftSerializer, CarSerializer, DriverSerializer, ExtraTaxSerializer, WriteRideSerializer, ReadRideSerializer
 
@@ -31,8 +33,11 @@ class ExtraTaxModelViewSet(ModelViewSet):
 
 
 class RideModelViewSet(ModelViewSet):
-    # queryset = Ride.objects.all()
     queryset = Ride.objects.select_related("driver", "car", "shift", "extra_tax")
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ("driver__name",)
+    ordering_fields = ("shift", "number")
+    filterset_fields = ("car__plate",)
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
